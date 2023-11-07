f6611185893e842a3ed825c03483e23ce7c3f470
Simontagbor <simontagbor360@gmail.com>
2023-11-07 16:49:17 +0000

    Add unauthorized endpoint to API

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """ Module of Index views
 """
-from flask import jsonify
+from flask import jsonify, abort
 from api.v1.views import app_views
-
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
@@ -13,7 +12,13 @@ def status() -> str:
     """
     return jsonify({"status": "OK"})
 
-
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+        - the status of the API
+    """
+    return jsonify(abort(401))
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
