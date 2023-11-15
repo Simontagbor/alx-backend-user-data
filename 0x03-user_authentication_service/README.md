d527c539107f3dd0bb179ab48b65742e4498aa1c
Simontagbor <simontagbor360@gmail.com>
2023-11-15 20:35:09 +0000

    add user model

---
+++ 0x03-user_authentication_service/README.md
@@ -0,0 +1,14 @@
+# 0x03. User authentication service
+`Backend` `python`
+
+
+## Learning Outcomes:
+In this project i learned how to :
+* declare API routes in a Flask app
+* get and set cookies
+* retrieve request form data
+* return various HTTP status codes
+
+
+## Tasks
+
---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Defines a model for user
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+# define user based on base
+
+
+class User(Base):
+    """
+    A simple class that creates users
+
+    Attributes:
+                id(int)
+                email(str)
+                hashed_password(str)
+                session_id(str)
+                reset_token(str)
+    """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    hashed_password = Column(String(250), nullable=False)
+    reset_token = Column(String(250), nullable=True)
