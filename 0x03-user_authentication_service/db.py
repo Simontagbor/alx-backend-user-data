f0796280996dcb2d4b1728970cf4056a96750b5d
Simontagbor <simontagbor360@gmail.com>
2023-11-16 17:16:51 +0000

    correctly type anotate DB.add_user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from typing import Type
 
 from user import Base, User
 
@@ -30,7 +31,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password):
+    def add_user(self, email: str, hashed_password: str) -> Type[User]:
         """
         Adds a User Instance to Database
 
