f92f96db409c640e15d8967e4bc0f5dbd1bbeebd
Simontagbor <simontagbor360@gmail.com>
2023-11-16 09:20:25 +0000

    add method for creating new user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,17 +6,19 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
-from user import Base
+from user import Base, User
 
 
 class DB:
     """DB class
     """
-    self._engine = create_engine("sqlite:///a.db", echo=True)
-    Base.metadata.drop_all(self._engine)
-    Base.metadata.create_all(self._engine)
-    
-    self.__session = None
+    def __init__(self) -> None:
+        """Initialize a new DB instance"""
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        
+        self.__session = None
 
     @property
     def _session(self) -> Session:
@@ -32,5 +34,8 @@ class DB:
         """ 
         returns a User object
         """
-
-
+        new_user = User(email=email, hashed_password=hashed_password)
+        session = self._session
+        session.add(new_user)
+        session.commit()
+        return new_user
