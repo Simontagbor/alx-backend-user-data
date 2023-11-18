27645eb9e184851f0729753261df7eca330a4ff4
Simontagbor <simontagbor360@gmail.com>
2023-11-18 20:42:16 +0000

    add login route for creating user session

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Defines a simple Flask Application"""
 
-from flask import request, Flask, jsonify
+from flask import request, Flask, jsonify, make_response, abort
 from auth import Auth
 from sqlalchemy.orm.exc import NoResultFound
 
@@ -34,6 +34,34 @@ def create_user():
     except NoResultFound:
         AUTH.register_user(email, password)
     return jsonify({"email": f"{email}", "message": "user created"}), 200
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """log user in and set session id
+
+    Return:
+        A json response object
+    """
+    if request.is_json:
+        data = request.get_json()
+    else:
+        data = request.form
+    email = data.get('email')
+    password = data.get('password')
+    try:
+        user = AUTH._db.find_user_by(email=email)
+        if AUTH.valid_login(email, password):
+            session_id = AUTH.create_session(email)
+            payload = {"email": f"{email}", "message": "logged in"}
+            response = make_response(str(payload))
+            response.set_cookie('session_id', session_id)
+            return response, 200
+        else:
+            print("email or password wrong")
+            abort(401)
+    except NoResultFound:
+        abort(401)
 
 
 if __name__ == "__main__":
