1c561388a90560e5035ae15e9d14be6b7c948780
Simontagbor <simontagbor360@gmail.com>
2024-01-12 19:08:31 +0000

    fix style error

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,14 +4,21 @@ import logging
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    """ Redacting sensitive info in logs """
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """Redacts sensitive info in logs."""
     for field in fields:
         message = re.sub(rf'{field}=[^;]*', f'{field}={redaction}', message)
     return message
 
+
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class """
+    """
+    Redacting Formatter class.
+    This class inherits from logging.Formatter
+    and redacts sensitive information
+    from logs.
+    """
 
     REDACTION: str = "***"
     FORMAT: str = ("[HOLBERTON] %(name)s %(levelname)s "
@@ -19,10 +26,16 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR: str = ";"
 
     def __init__(self, fields: List[str]):
+        """
+        Initializes the RedactingFormatter class.
+        """
         super().__init__(self.FORMAT)
         self.fields: List[str] = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """
+        Formats the record after redacting sensitive information.
+        """
         record.msg = filter_datum(self.fields, self.REDACTION,
                                   record.msg, self.SEPARATOR)
         return super().format(record)
