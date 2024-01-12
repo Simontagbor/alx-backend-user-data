e7ea8205fdf175b259fa16e8ff4a4ef834f9e7bf
Simontagbor <simontagbor360@gmail.com>
2024-01-12 19:45:46 +0000

    add function for retrieving logs

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,7 +2,9 @@
 """This module contains the RedactingFormatter class."""
 import re
 import logging
-from typing import List
+from typing import List, Tuple
+
+PII_FIELDS: Tuple[str, ...] = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -11,6 +13,23 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     for field in fields:
         message = re.sub(rf'{field}=[^;]*', f'{field}={redaction}', message)
     return message
+
+
+def get_logger() -> logging.Logger:
+    """gets a logger object"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(stream_handler)
+
+    file_handler = logging.FileHandler("user_data.log")
+    file_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(file_handler)
+
+    return logger
 
 
 class RedactingFormatter(logging.Formatter):
