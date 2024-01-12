af81ce0f65d46757964a6c23f8e333e4522d8a50
Simontagbor <simontagbor360@gmail.com>
2024-01-12 18:49:01 +0000

    update filtered_logger with RedactingFormatter Class

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,14 +1,28 @@
-#!/usr/bin/env python3
-"""Using Regular expressions to redact personal data"""
-
 import re
-from typing import List
+import logging
 
 
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
-    """returns the log message obfuscated"""
+def filter_datum(fields, redaction, message, separator):
+    """ Redacting sensitive info in logs """
     for field in fields:
-        message = re.sub(f"{field}=.*?{separator}",
-                         f"{field}={redaction}{separator}", message)
+        message = re.sub(rf'{field}=.+?{separator}',
+                         f'{field}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = ("[HOLBERTON] %(name)s %(levelname)s "
+              "%(asctime)-15s: %(message)s")
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        # filter values in record
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.msg, self.SEPARATOR)
