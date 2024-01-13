9f3abfeba10d2adf7441d45f0d47cf6db1c9877a
Simontagbor <simontagbor360@gmail.com>
2024-01-13 04:45:31 +0000

    add script to check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,3 +9,9 @@ def hash_password(password: str) -> bytes:
       hashed password, which is a byte string.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """This function validates that the provided
+      password matches the hashed password.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
