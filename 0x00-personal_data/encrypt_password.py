1520b94ab14b86f4e4d08e1b3e48a77e31caf3d3
Simontagbor <simontagbor360@gmail.com>
2024-01-13 04:35:00 +0000

    add password emcryption

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""This module contains the hash_password function."""
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """This function returns a salted, hashed password, which is a byte string."""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
