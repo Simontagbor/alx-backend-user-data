0f773cb9466ca6016594aeb7473f1aab4952a099
Simontagbor <simontagbor360@gmail.com>
2024-01-14 00:35:16 +0000

    fix pep8 error

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -23,7 +23,7 @@ class Auth:
         """
         if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
-    
+
         for excluded_path in excluded_paths:
             if fnmatch.fnmatch(path, excluded_path):
                 return False
